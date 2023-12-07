35fc0d3482d8aa1f7abbe0e946921d13071a29d9
Fengdaidai6 <127473859+Fengdaidai6@users.noreply.github.com>
2023-12-07 15:58:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ohT8t65scPPqdEwb2wKF7pz3m8UY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'XD2MblahLe85XU3jIUiI2BLPo1KynjTRgsZhfaxAG_s',
+      useTemplateId: 'ky0nIJoxEas4FMFqSCo369v8Z2Wi-sfzkSHojEjB8TM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-22',
       festivals: [
