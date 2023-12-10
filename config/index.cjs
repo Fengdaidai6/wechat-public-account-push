91f2818842165989c67250696493e480d5ece250
Fengdaidai6 <127473859+Fengdaidai6@users.noreply.github.com>
2023-12-10 09:19:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '阿云宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ohT8t65scPPqdEwb2wKF7pz3m8UY',
+      id: 'ohT8t648b8Kjli-TKno0PF3DMRpE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '_VoiEkgbv-po5v8_DqbJfDOSJ7M8D8f5pcQL6jarfnI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '_VoiEkgbv-po5v8_DqbJfDOSJ7M8D8f5pcQL6jarfnI',
+  CALLBACK_TEMPLATE_ID: 'Ji-WJvXlP4QwDJpE6xsrWyxqkPtnjTH7ikScW7jl49k',
 
   CALLBACK_USERS: [
     {
